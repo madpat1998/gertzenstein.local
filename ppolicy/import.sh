d6a93d3b8997adf240c01bafd1601591a9295e0b
Nyno-Kun <lordswiss@swisshero.net>
2018-10-21 10:53:11 +0200

    Disabled user now dont have passwords

--- ppolicy/import.sh
+++ ppolicy/import.sh
@@ -5,5 +5,9 @@ ldapadd -D cn=admin,dc=gertzenstein,dc=local -w $password -f policies.ldif
 ldapadd -Y EXTERNAL -H ldapi:///  -f ppmodule.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f ppolicyoverlay.ldif
 ldapadd -D cn=admin,dc=gertzenstein,dc=local -w $password -f passwordpolicy.ldif
+ldapadd -Q -Y EXTERNAL -H ldapi:/// -f memberof_config.ldif
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f refint1.ldif
+ldapadd -Q -Y EXTERNAL -H ldapi:/// -f refint2.ldif
+
 
 
